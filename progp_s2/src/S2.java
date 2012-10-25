cbfff5d6da99cd949d3973af2f2c035528598f60
Dennis <dennis.ekstrom@gmail.com>
2012-10-25 11:07:21 +0200

    commit2

--- progp_s2/src/S2.java
+++ progp_s2/src/S2.java
@@ -6,51 +6,47 @@ import java.util.List;
 import java.util.Stack;
 
 public class S2 {
-	
-//	class EndSym {
-//		String sym;
-//		Integer val;
-//		
-//		EndSym(String sym, Integer val) {
-//			this.sym = sym;
-//			this.val = val;
-//		}
-//	}
-	
+
+	// class EndSym {
+	// String sym;
+	// Integer val;
+	//
+	// EndSym(String sym, Integer val) {
+	// this.sym = sym;
+	// this.val = val;
+	// }
+	// }
+
 	private Stack<String> symbols;
 	private int val;
-	
+
 	public int eval(String str) {
 		List<String> subStrs = Arrays.asList(str.split("\\s+"));
 		Collections.reverse(subStrs);
 		symbols = new Stack<String>();
 		symbols.addAll(subStrs);
-		
-//		for(int i = subStrs.length; i >= 0; i--) {
-//			symbols.push(subStrs[i]));
-//		}		
 	}
-	
+
 	private void popAndAssign(int val) {
 		symbols.pop().val = val;
 	}
-	
+
 	private void dansk() {
 		String es = symbols.peek();
-		
+
 		if (es.sym.equals("nul"))
 			es.val = 0;
 		else
 			en2ni();
 	}
-	
+
 	private void en2ni() {
 		EndSym es = symbols.peek();
-		
+
 		if (es.sym.equals("en"))
 			symbols.pop();
-			es.val = 1;
-			mer();
+		es.val = 1;
+		mer();
 		else if (es.sym.equals("et"))
 			es.val = 1;
 		else if (es.sym.equals("to"))
@@ -72,10 +68,10 @@ public class S2 {
 		else
 			ti2nitten();
 	}
-	
+
 	private void ti2nitten() {
 		EndSym es = symbols.peek();
-		
+
 		if (es.sym.equals("ti"))
 			es.val = 10;
 		else if (es.sym.equals("ellve"))
@@ -99,49 +95,49 @@ public class S2 {
 		else
 			tyve2halvfems();
 	}
-	
-	
-	
-	
-}
-
-//dansk	→	nul
-//|	en2ni	mer
-//en2ni	→	en	mer
-//|	et	mer
-//|	to	mer
-//|	tre	mer
-//|	fire	mer
-//|	fem	mer
-//|	seks	mer
-//|	syv	mer
-//|	otte	mer
-//|	ni	mer
-//|	ti2nitten
-//ti2nitten	→	ti
-//|	ellve
-//|	tolv
-//|	tretten
-//|	fjorten
-//|	femten
-//|	seksten
-//|	sytten
-//|	atten
-//|	nitten
-//|	tyve2halvfems
-//mer	 →	og tyve2halvfems	(+ tiotal t, 20 ≤ t ≤ 90)
-//|	ε	( 0, dvs enbart en2ni)
-//tyve2halvfems	 →	tyve	(20)
-//|	tredive	(30)
-//|	fyrre	(40)
-//|	fyrretyve	(40)
-//|	halvtreds2halvfems emfas	(tiotal t, 50 ≤ t ≤ 90)
-//halvtreds2halvfems	→	halv halvmult	(-0,5+...)
-//|	 mult	(...)
-//mult	 →	tres	3)
-//|	firs	4)
-//|	fjerds	4)
-//halvmult	 →	mult	
-//|	fems	5)
-//emfas	→	sindtyve	(× 20)
-//|	 ε	(× 20)
+	//
+	//
+	//
+	//
+	// }
+	//
+	// //dansk → nul
+	// //| en2ni mer
+	// //en2ni → en mer
+	// //| et mer
+	// //| to mer
+	// //| tre mer
+	// //| fire mer
+	// //| fem mer
+	// //| seks mer
+	// //| syv mer
+	// //| otte mer
+	// //| ni mer
+	// //| ti2nitten
+	// //ti2nitten → ti
+	// //| ellve
+	// //| tolv
+	// //| tretten
+	// //| fjorten
+	// //| femten
+	// //| seksten
+	// //| sytten
+	// //| atten
+	// //| nitten
+	// //| tyve2halvfems
+	// //mer → og tyve2halvfems (+ tiotal t, 20 ≤ t ≤ 90)
+	// //| ε ( 0, dvs enbart en2ni)
+	// //tyve2halvfems → tyve (20)
+	// //| tredive (30)
+	// //| fyrre (40)
+	// //| fyrretyve (40)
+	// //| halvtreds2halvfems emfas (tiotal t, 50 ≤ t ≤ 90)
+	// //halvtreds2halvfems → halv halvmult (-0,5+...)
+	// //| mult (...)
+	// //mult → tres 3)
+	// //| firs 4)
+	// //| fjerds 4)
+	// //halvmult → mult
+	// //| fems 5)
+	// //emfas → sindtyve (× 20)
+	// //| ε (× 20)
